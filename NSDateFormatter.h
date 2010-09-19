c4d55c218e331559a0f2d69f38a11b8e7c9f22d8
Johan Ruokangas <johan@jozan.fi>
2010-09-19 03:59:26 +0300

    unnecessary files got removed

--- NSDateFormatter.h
+++
@@ -1,9 +0,0 @@
-//
-//  NSDateFormatter.h
-//  NordicLeague Tool
-//
-//  Created by Johan Ruokangas on 12.8.2010.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
-//
-
-#import <Cocoa/Cocoa.h>
